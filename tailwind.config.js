80847585f93d8de8909954300db25be69a977654
Vishal <vishalmeharwade1@gmail.com>
2024-12-19 09:02:57 +0530

    export error resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html", // Include the Vite entry point
     "./src/**/*.{js,jsx}", // Include all your React components
@@ -9,6 +9,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-
-
